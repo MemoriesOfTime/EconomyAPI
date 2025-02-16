9c34d479f3cbf33322751766b405edeac42e8a9b
Mcayear <mcayear@163.com>
2025-02-16 16:07:26 +0800

    refactor: remove unused language files, update README.

--- src/main/java/me/onebone/economyapi/config/UpgradeConfig.java
+++ src/main/java/me/onebone/economyapi/config/UpgradeConfig.java
@@ -13,7 +13,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Scanner;
